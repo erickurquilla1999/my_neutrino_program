865e463cea4bdb23d96b047e5c24669fc35c11e3
erickurquilla1999 <erickurquilla1999@gmail.com>
2022-07-15 00:27:37 -0600

    writting input parameter in IS units

--- source/input_parameters.jl
+++ source/input_parameters.jl
@@ -1,31 +1,37 @@
+include("constants.jl")
+
 # grid parameters
 number_of_cells=1
-cell_x_lenght=1e2 #cm
+cell_x_lenght=1e10 # m
 
-particles_number_density=4.891290819e+35 #cm-1
-perturbation_amplitud=1.0e-6
+particles_number_density=0.0 # m^(-3)
+perturbation_amplitud=0.0
 
-background_electron_number_density=0.0
-background_antielectron_number_density=0.0
+background_electron_number_density=1e32 # m^(-3)
+background_antielectron_number_density=0.0 # m^(-3)
 
-theta_12=14.0*pi/180
-theta_13=29.0*pi/180
-theta_23=27.0*pi/180
+#mixing and CP phase angles numerical values in degrees             
+theta_12 = 39.0  * pi/180
+theta_13 = 29.0  * pi/180
+theta_23 = 27.0  * pi/180
+delta_cp = 220.0 * pi/180
+   
+#mass numerical values in eV/c^2               
+mass_1=0.01 * eV_to_J / c2 # kg 
+mass_2=0.03 * eV_to_J / c2 # kg 
+mass_3=0.02 * eV_to_J / c2 # kg
 
-delta_cp=220*pi/180
+#energy numerical value in eV              
+neutrino_energy=50.0e+6 * eV_to_J # J
 
-mass_1=0.01 #eV/c2
-mass_2=0.03 #eV/c2
-mass_3=0.02 #eV/c2
-
-neutrino_energy=50.0e+6 #eV
-
+#number of neutrino flavor to be simulated
 number_of_flavors=2
 
 #simulation parameters
-simulation_initial_time=0.0
-time_step=1e-5
+simulation_initial_time=0.0 # s
+time_step=0.0005169584630893724/100 # s
 number_of_steps=200
+
 
 
 
